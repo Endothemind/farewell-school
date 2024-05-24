66ed7a8e2645b44c959244d650844e602d5d963d
Endothemind <149655841+Endothemind@users.noreply.github.com>
2024-05-24 17:16:32 -0500

    Update reset.js

--- scripts/reset.js
+++ scripts/reset.js
@@ -1,15 +1,15 @@
 function rebootReset() {
   game.rebootNum = D(0);
   game.durability = D(1).mul(game.researchLevel[5]+1);
-  if (!game.programActive[4]) {
+  if (!game.programActive[1]) {
     for (var i = 0; i < game.programActive.length; i++) {
       game.programActive[i] = 0;
     }
     game.base = D(2);
   }
-  if (!game.programActive[4] || game.shopBought[2] < 2) game.digits = D(1);
+  if (!game.programActive[4] || game.shopBought[2] < 2) game.digits = D(1000);
   if (!game.programActive[4] || game.shopBought[2] < 2) game.number = D(0);
-  if (!game.programActive[4] || game.shopBought[2] < 3) game.money = D(0);
+  if (!game.programActive[4] || game.shopBought[2] < 3) game.money = D(555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555);
   if (!game.programActive[4] || game.shopBought[2] < 3) game.shopBought[5] = 0;
   game.rebootTime = new Date().getTime();
 }
